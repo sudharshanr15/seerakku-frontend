b2468e1d523725b5b7c6059ed7bd064f54d96682
Uwais <muhammaduuwais132@gmail.com>
2024-12-23 21:22:48 +0530

    What We Do Page

---
+++ src/features/whatwedo/components/Hero.tsx
@@ -0,0 +1,9 @@
+import HeroContainer from '@/components/HeroContainer';
+
+const Hero = () => {
+    return(
+        <HeroContainer title='WHAT WE DO' desc="At Seerakku, we’ve initiated several impactful programs and projects to create a positive ecological footprint" />
+    )
+}
+
+export default Hero;
