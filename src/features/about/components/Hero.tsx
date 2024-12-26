8a6c39b82187839131f5a4bf264decc41e3c4da5
Sudharshan <sudharshanrms2004@gmail.com>
2024-12-26 14:32:40 +0530

    Used global components for about us hero component

--- src/features/about/components/Hero.tsx
+++ src/features/about/components/Hero.tsx
@@ -1,14 +1,16 @@
+import PageHero from "@/components/PageHero";
+import HeroDescription from "@/components/PageHero/Description";
+import HeroHeading from "@/components/PageHero/Heading";
+
 function Hero() {
   return (
-    <section className="bg-black">
-      <div className="layout-section-sm text-center">
-        <h1 className="heading-1 mb-4 text-white">About Us</h1>
-        <p className="text-lg text-center text-white">
-          Through our efforts, we strive to create a sustainable future for all
-          and mitigate the effects of climate change.
-        </p>
-      </div>
-    </section>
+    <PageHero>
+      <HeroHeading>About Us</HeroHeading>
+      <HeroDescription>
+        Through our efforts, we strive to create a sustainable future for all
+        and mitigate the effects of climate change.
+      </HeroDescription>
+    </PageHero>
   );
 }
 
