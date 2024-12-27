5b253abb2bf72909d8b9a260e3e17d43f6201512
Uwais <muhammaduuwais132@gmail.com>
2024-12-27 20:13:14 +0530

    What we do Responsive

--- src/components/HeroContainer.jsx
+++ src/components/HeroContainer.jsx
@@ -1,8 +1,8 @@
 export default function HeroContainer({ title, desc }) {
   return (
-    <section className="bg-black py-32">
+    <section className="bg-black py-32 px-5">
       <div className="text-center max-w-[540px] mx-auto">
-        <h1 className=" heading-1 mb-4 text-white">{title}</h1>
+        <h1 className=" heading-2 sm:heading-1 mb-4 text-white">{title}</h1>
         <p className="text-lg text-center text-white">{desc}</p>
       </div>
     </section>
