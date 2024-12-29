478c7bcfe274b3499434e416705e23577eee8aac
Sudharshan <sudharshanrms2004@gmail.com>
2024-12-29 15:21:14 +0530

    onClick prop added to button component

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,3 +1,4 @@
+import { MouseEventHandler } from "react";
 import { ButtonFill, ButtonOutline, ButtonType } from "./type";
 
 const Button = ({
@@ -5,18 +6,21 @@ const Button = ({
   children,
   className,
   outline,
+  onClick,
   ...props
 }: {
   type: ButtonType;
   children: React.ReactNode;
   className?: string;
   outline?: boolean;
+  onClick?: MouseEventHandler;
 }) => {
   const class_name = outline ? ButtonOutline[type] : ButtonFill[type];
 
   return (
     <button
       className={`py-3 px-5 ${class_name} uppercase ${className} text-lg font-big-noodle-titling`}
+      onClick={onClick}
       {...props}
     >
       {children}
