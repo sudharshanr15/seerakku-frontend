a9c32fc346057bff17f467b1b03664f4703c63b7
Uwais <muhammaduuwais132@gmail.com>
2025-01-08 11:38:15 +0530

    Button and Scaling Corrections

--- src/features/home/components/ValuableCount.tsx
+++ src/features/home/components/ValuableCount.tsx
@@ -19,36 +19,36 @@ const ValuableCount = () => {
           {/* <Button type="primary">View More</Button> */}
         </div>
         <div className="w-full xl:w-2/3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 flex-shrink-0">
-          <div className="odd:bg-primary-green even:bg-secondary-forest hover:scale-105 transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
+          <div className="odd:bg-primary-green even:bg-secondary-forest transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
             <Image src={tree} alt="Trees Planted"></Image>
             <h4 className="text-white heading-1 mt-2 !text-80">400</h4>
             <p className="text-white">Trees Planted</p>
           </div>
-          <div className="odd:bg-primary-green even:bg-secondary-forest hover:scale-105 transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
+          <div className="odd:bg-primary-green even:bg-secondary-forest transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
             <Image src={carbon} alt="Carbon Offseted"></Image>
 
             <h4 className="text-white heading-1 mt-2 !text-80">8M+</h4>
             <p className="text-white">Carbon Offseted</p>
           </div>
-          <div className="odd:bg-primary-green even:bg-secondary-forest hover:scale-105 transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
+          <div className="odd:bg-primary-green even:bg-secondary-forest transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
             <Image src={tree} alt="Communities Empowered"></Image>
 
             <h4 className="text-white heading-1 mt-2 !text-80">400</h4>
             <p className="text-white">Communitites Empowered</p>
           </div>
-          <div className="odd:bg-primary-green even:bg-secondary-forest hover:scale-105 transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
+          <div className="odd:bg-primary-green even:bg-secondary-forest transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
             <Image src={hours} alt="Volunteering Hours"></Image>
 
             <h4 className="text-white heading-1 mt-2 !text-80">2190</h4>
             <p className="text-white">Total Volunteering Hours</p>
           </div>
-          <div className="odd:bg-primary-green even:bg-secondary-forest hover:scale-105 transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
+          <div className="odd:bg-primary-green even:bg-secondary-forest transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
             <Image src={water} alt="Water Conserved"></Image>
 
             <h4 className="text-white heading-1 mt-2 !text-80">1M</h4>
             <p className="text-white">Water Conserved</p>
           </div>
-          <div className="odd:bg-primary-green even:bg-secondary-forest hover:scale-105 transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
+          <div className="odd:bg-primary-green even:bg-secondary-forest transition duration-150 p-5 text-center text-white flex flex-col justify-center items-center xl:aspect-square">
             <Image src={plastic} alt="Plastics Collected"></Image>
 
             <h4 className="text-white heading-1 mt-2 !text-80">2T</h4>
