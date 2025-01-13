380c32b79fca16413aa0a1f1fcf1b425827ba6c6
Uwais <muhammaduuwais132@gmail.com>
2025-01-13 21:11:46 +0530

    Images update 1

--- src/features/our_focus/components/FocusList.tsx
+++ src/features/our_focus/components/FocusList.tsx
@@ -4,7 +4,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 import Image, { StaticImageData } from "next/image";
-import { image_1, image_2, image_3, image_4, image_5 } from "../assets";
+import { climate, clean, lifebelowwater, image_4, image_5 } from "../assets";
 
 type FocusListType = {
   title: string;
@@ -16,27 +16,27 @@ const focus_list: FocusListType[] = [
   {
     title: "No Poverty",
     desc: "We provide agroforestry opportunities for farmers to improve their livelihoods and promote sustainable land use practices.",
-    image: image_1,
+    image: image_4,
   },
   {
     title: "Clean Water & Sanitation",
     desc: "We work towards restoring water sources through reforestation and watershed management initiatives.",
-    image: image_2,
+    image: clean,
   },
   {
     title: "Zero Hunger",
     desc: "Our afforestation initiatives provide food and habitat for wildlife, while our agroforestry programs promote sustainable farming practices and help increase crop yields.",
-    image: image_3,
+    image: image_5,
   },
   {
     title: "Climate Action",
     desc: "Seerakku primarily focuses on climate action. We plant trees, promote sustainable practices, and engage in coastal plantations to reduce carbon footprint and offset emissions.",
-    image: image_4,
+    image: climate,
   },
   {
     title: "Life Below Water",
     desc: "Seerakku is committed to promoting coastal plantation and marine conservation initiatives to protect and preserve marine ecosystems.",
-    image: image_5,
+    image: lifebelowwater,
   },
   {
     title: "Partnership for the Goals",
