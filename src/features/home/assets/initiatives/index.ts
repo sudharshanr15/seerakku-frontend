f7541aec60a391749ac6140806ae4fbf96c23910
Sudharshan <sudharshanrms2004@gmail.com>
2025-01-13 17:54:44 +0530

    New card added to initiatives component

--- src/features/home/assets/initiatives/index.ts
+++ src/features/home/assets/initiatives/index.ts
@@ -3,3 +3,4 @@ export { default as initiative_2 } from "./2.jpg";
 export { default as initiative_3 } from "./3.jpg";
 export { default as initiative_4 } from "./4.jpg";
 export { default as initiative_5 } from "./5.png";
+export { default as initiative_6 } from "./6.jpg";
--- src/features/home/components/Initiatives.tsx
+++ src/features/home/components/Initiatives.tsx
@@ -4,26 +4,50 @@ import {
   initiative_3,
   initiative_4,
   initiative_5,
+  initiative_6,
 } from "../assets";
 import { InitiativesListType } from "../types";
 import InitiativesItem from "./InitiativesItem";
 import Button from "@/components/buttons/Button";
 import Link from "next/link";
 
-const initiatives_list_1: InitiativesListType[] = [
+// const initiatives_list_1: InitiativesListType[] = [
+//   {
+//     title: "Community Engagement and Corporate Volunteering",
+//     desc: "Foster community participation in conservation efforts.",
+//     image: initiative_1,
+//   },
+//   {
+//     title: "Reforestation and Afforestation Programs",
+//     desc: "Increase green cover in urban and rural areas while enhancing biodiversity.",
+//     image: initiative_2,
+//   },
+// ];
+
+// const initiatives_list_2: InitiativesListType[] = [
+//   {
+//     title: "Eco-Education and Awareness",
+//     desc: "Inspire a culture of environmental responsibility.",
+//     image: initiative_3,
+//   },
+//   {
+//     title: "Wildlife Conservation",
+//     desc: "Protect and restore wildlife habitats. ",
+//     image: initiative_5,
+//   },
+//   {
+//     title: "Sustainable Waste Management",
+//     desc: "Turn waste into a resource through eco-friendly solutions.",
+//     image: initiative_4,
+//   },
+// ];
+
+const initiatives_list: InitiativesListType[] = [
   {
     title: "Community Engagement and Corporate Volunteering",
     desc: "Foster community participation in conservation efforts.",
     image: initiative_1,
   },
-  {
-    title: "Reforestation and Afforestation Programs",
-    desc: "Increase green cover in urban and rural areas while enhancing biodiversity.",
-    image: initiative_2,
-  },
-];
-
-const initiatives_list_2: InitiativesListType[] = [
   {
     title: "Eco-Education and Awareness",
     desc: "Inspire a culture of environmental responsibility.",
@@ -35,10 +59,20 @@ const initiatives_list_2: InitiativesListType[] = [
     image: initiative_5,
   },
   {
+    title: "Reforestation and Afforestation Programs",
+    desc: "Increase green cover in urban and rural areas while enhancing biodiversity.",
+    image: initiative_2,
+  },
+  {
     title: "Sustainable Waste Management",
     desc: "Turn waste into a resource through eco-friendly solutions.",
     image: initiative_4,
   },
+  {
+    title: "Urban Green Spaces Creation",
+    desc: "Develop small green patches in urban areas to improve air quality and aesthetics.",
+    image: initiative_6
+  }
 ];
 
 const Initiatives = () => {
@@ -53,7 +87,7 @@ const Initiatives = () => {
           programs:
         </p>
       </div>
-      <div className="flex flex-col md:flex-row xl:flex-row gap-6">
+      {/* <div className="flex flex-col md:flex-row xl:flex-row gap-6">
         <div className="w-full md:w-[49%] xl:w-1/3">
           <div className="flex flex-col gap-6 h-full">
             {initiatives_list_1.map((ele, index) => (
@@ -64,26 +98,6 @@ const Initiatives = () => {
         <div className="w-full md:w-1/2 xl:w-2/3">
           <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
             {initiatives_list_2.map((ele, index) => (
-              // <div
-              //   className={`relative ${initiatives_list_2.length == index + 1 && index % 2 == 0 ? "last:col-span-2" : ""}`}
-              //   key={index}
-              // >
-              //   <div className="w-full">
-              //     <Image
-              //       src={ele.image}
-              //       alt="Get Involved"
-              //       className="w-full object-contain"
-              //     />
-              //   </div>
-              //   <ImageTint>
-              //     <div className="flex items-end p-7 h-full">
-              //       <div className="text-white">
-              //         <h3 className="heading-3">{ele.title}</h3>
-              //         <p>{ele.desc}</p>
-              //       </div>
-              //     </div>
-              //   </ImageTint>
-              // </div>
               <InitiativesItem
                 item={ele}
                 key={index}
@@ -96,49 +110,35 @@ const Initiatives = () => {
             ))}
           </div>
         </div>
-      </div>
+      </div> */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+          {initiatives_list.map((ele, ele_index) => {
+            const item_length = initiatives_list.length;
+            let span_length: string = "";
+            if (item_length == ele_index + 1) {
+              switch (ele_index % 3) {
+                case 0:
+                  span_length = "last:col-span-full";
+                  break;
+                case 1:
+                  span_length = "last:sm:col-span-2";
+                  break;
+                case 2:
+                  break;
+              }
+            }
+            return (
+              <div className={`w-full ${span_length}`} key={ele_index}>
+                  <InitiativesItem item={ele} className="flex-grow" />
+              </div>
+            );
+          })}
+        </div>
       <div className=" flex justify-center mt-10">
         <Link href={"/initiatives"}>
           <Button type="primary">See More</Button>
         </Link>
       </div>
-      {/* <div className="grid grid-cols-3 gap-6">
-        {initiatives_list.map((ele, index) => {
-          const length = initiatives_list.length;
-          let col_span;
-          if (length == index + 1) {
-            switch (index % 3) {
-              case 0:
-                col_span = "last:col-span-3";
-                break;
-              case 1:
-                col_span = "last:col-span-2";
-                break;
-              case 2:
-                break;
-            }
-          }
-          return (
-            <div className={`relative ${col_span}`} key={index}>
-              <div className="w-full h-full">
-                <Image
-                  src={ele.image}
-                  alt="Get Involved"
-                  className="w-full h-full object-cover"
-                />
-              </div>
-              <ImageTint>
-                <div className="flex items-end p-7 h-full">
-                  <div className="text-white">
-                    <h3 className="heading-3">{ele.title}</h3>
-                    <p>{ele.desc}</p>
-                  </div>
-                </div>
-              </ImageTint>
-            </div>
-          );
-        })}
-      </div> */}
     </section>
   );
 };
