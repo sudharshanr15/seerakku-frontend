9940a620b4fec6baa30503f0c39c457686efcc25
Sudharshan <sudharshanrms2004@gmail.com>
2025-06-05 18:41:35 +0530

    Partners list added

--- src/features/home/components/Partners.tsx
+++ src/features/home/components/Partners.tsx
@@ -1,69 +1,146 @@
 "use client";
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 import beyond from '../assets/partners/beyond sustainability.svg'
 import kauvery from '../assets/partners/kauvery.svg'
 import Gro from '../assets/partners/Gro.png'
+import * as partners from "../assets/partners_new"
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import Link from 'next/link'
 
+const partners_list: {
+  name: string,
+  image: StaticImageData
+}[] = [
+  {
+    name: "Access Healthcare",
+    image: partners.access_healthcare
+  },
+  {
+    name: "Amazon",
+    image: partners.amazon
+  },
+  {
+    name: "Beyond Sustainability",
+    image: partners.beyond_sustainability
+  },
+  {
+    name: "BHEL",
+    image: partners.bhel
+  },
+  {
+    name: "Bhumi",
+    image: partners.bhumi
+  },
+  {
+    name: "BNI",
+    image: partners.bni
+  },
+  {
+    name: "CCAG",
+    image: partners.ccag
+  },
+  {
+    name: "DBS",
+    image: partners.beyond_sustainability
+  },
+  {
+    name: "Deloitte",
+    image: partners.deloitte
+  },
+  {
+    name: "Freshworks",
+    image: partners.freshworks
+  },
+  {
+    name: "Goodera",
+    image: partners.goodera
+  },
+  {
+    name: "Greater Chennai Corporation",
+    image: partners.gcc
+  },
+  {
+    name: "Green Tamilnadu",
+    image: partners.green_tn
+  },
+  {
+    name: "GRO Foundation",
+    image: partners.gro_foundation
+  },
+  {
+    name: "Grow Billion Trees",
+    image: partners.grow_billion_trees
+  },
+  {
+    name: "Grow Trees",
+    image: partners.grow_trees
+  },
+  {
+    name: "Kaleesuwari",
+    image: partners.kaleesuwari
+  },
+  {
+    name: "Kauvery Hospital",
+    image: partners.kauvery_hospital
+  },
+  {
+    name: "MGM Healthcare",
+    image: partners.mgm_healthcare
+  },
+  {
+    name: "Nirmaan Organization",
+    image: partners.nirmaan_organisation
+  },
+  {
+    name: "Rotary",
+    image: partners.rotary
+  },
+  {
+    name: "Ruva Foundation",
+    image: partners.ruva_foundation
+  },
+  {
+    name: "Saytrees",
+    image: partners.saytrees
+  },
+  {
+    name: "Sustainable Development Council",
+    image: partners.sdc
+  },
+  {
+    name: "The Executive Center",
+    image: partners.the_executive_center
+  },
+  {
+    name: "United Way of Chennai",
+    image: partners.united_way_of_chennai
+  },
+  {
+    name: "University of Madras",
+    image: partners.university_of_madras
+  },
+  {
+    name: "Wells Fargo",
+    image: partners.wellsfargo
+  }
+]
+
 const Partners = () => {
   return (
     <section className="layout-section-lg text-center">
       <h2 className="text-center heading-2">Our Partner</h2>
-      <Swiper
-        spaceBetween={30}
-        slidesPerView={1}
-        breakpoints={{
-          440: {
-            slidesPerView: 2,
-          },
-          640: {
-            slidesPerView: 3,
-          },
-          768: {
-            slidesPerView: 3,
-          },
-          1024: {
-            slidesPerView: 3,
-          },
-        }}
-        modules={[Pagination]}
-        pagination={{
-          el: ".partner-pagination",
-          clickable: true,
-          type: "bullets",
-        }}
-      >
-        {/*  */}
-        <SwiperSlide>
-
-        <Link href={'https://beyondsustainability.in/'}>
-            <Image className=' w-full h-36 object-contain' src={beyond} alt='Beyond Sustainability'></Image>
-        </Link>
-
-        </SwiperSlide>
-        
-        <SwiperSlide>
-          <Link href={'https://www.kauveryhospital.com/'}>
-        <Image className=' w-full h-36 object-contain' src={kauvery} alt='Kauvery'></Image>
-          </Link>
-
-        </SwiperSlide>
-        
-        <SwiperSlide>
-          <Link href={'https://gro-withus.com/'}>
-        <Image className=' w-full h-36 object-contain' src={Gro} alt='GroWithUs'></Image>
-          </Link>
-        </SwiperSlide>
-        
-        
-        <div className="mt-3">
-          <div className="partner-pagination"></div>
-        </div>
-      </Swiper>
+      <div className="mt-4 grid grid-cols-4 gap-4 items-center">
+        {
+          partners_list.map((e, index) => (
+            <div className='text-center' key={index}>
+              <Image className="w-[128px] h-auto mx-auto" src={e.image} alt={e.name} />
+            </div>
+          ))
+        }
+      </div>
     </section>
   );
 };
