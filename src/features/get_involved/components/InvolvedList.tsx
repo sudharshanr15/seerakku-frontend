5071d494303b5bf20a03d627cb6807712d5d99ff
Sudharshan <sudharshanrms2004@gmail.com>
2025-06-05 11:04:03 +0530

    Get involved page content replaced

--- src/features/get_involved/components/InvolvedList.tsx
+++ src/features/get_involved/components/InvolvedList.tsx
@@ -23,21 +23,24 @@ function InvolvedList() {
             <div className="relative md:right-14 top-0 w-full">
               <ShadowContainer shadowTo="right" theme={ColorType.primary_green}>
                 <div className="p-10">
-                  <h2 className="heading-2">Become A Volunteer</h2>
-                  <p className="pb-4">
-                    Join our dedicated team of volunteers and actively
-                    participate in our initiatives.
+                  <h2 className="heading-2">Be the Change You Want to See in the World</h2>
+                  <p className="mb-4">
+                    Join Seerakku’s mission to protect our environment and create a sustainable future! As a volunteer, you’ll be part of a community working toward impactful initiatives, including tree plantation, waterbody restoration, plastic collection drives, and eco-education programs.
                   </p>
-                  <p className="pb-4">
-                    Whether it's tree planting, water restoration, coastal
-                    plantation, or agroforestry, your time and effort can make a
-                    significant difference.
-                  </p>
-                  <p className="pb-4">
-                    As a volunteer, you'll have the opportunity to connect with
-                    nature, learn new skills, and contribute directly to the
-                    well-being of our planet.
-                  </p>
+                  <div className="mb-4">
+                    <p className="font-bold mb-2">Why Volunteer with Us?</p>
+                    <ul className="list-inside list-disc">
+                      <li key={"volunteer-list-1"}>
+                        Make a tangible impact on the environment.
+                      </li>
+                      <li key={"volunteer-list-2"}>
+                        Learn eco-friendly practices and sustainable solutions.
+                      </li>
+                      <li key={"volunteer-list-3"}>
+                        Connect with like-minded individuals and organizations.
+                      </li>
+                    </ul>
+                  </div>
                   <ButtonLink type="primary_green" href={BECOME_VOLUNTEER_LINK}>Become A Volunteer</ButtonLink>
                 </div>
               </ShadowContainer>
@@ -59,20 +62,27 @@ function InvolvedList() {
             <div className="relative md:-right-14 top-0 w-full">
               <ShadowContainer shadowTo="left" theme={ColorType.primary}>
                 <div className="p-10">
-                  <h2 className="heading-2">Become A Donor</h2>
-                  <p className="pb-4">
-                    Support our organization financially by making a donation.
+                  <h2 className="heading-2">Your Contribution Fuels Change</h2>
+                  <p className="mb-4">
+                    Every rupee you donate helps us restore ecosystems, empower communities, and combat climate change. From planting trees to supporting underprivileged farmers, your generosity drives our mission forward.
                   </p>
-                  <p className="pb-4">
-                    Your contribution, no matter the size, directly funds our
-                    initiatives and helps us implement projects that promote
-                    sustainability, biodiversity, and environmental
-                    conservation.
-                  </p>
-                  <p className="pb-4">
-                    Your generosity enables us to continue our important work
-                    and create a positive impact on the planet.
-                  </p>
+                  <div className="mb-4">
+                    <p className="font-bold mb-2">Where Your Donations Go:</p>
+                    <ul className="list-inside list-disc">
+                      <li key={"donate-list-1"}>
+                        Reforestation and afforestation projects.
+                      </li>
+                      <li key={"donate-list-2"}>
+                        Agroforestry initiatives to uplift marginalized farmers.
+                      </li>
+                      <li key={"donate-list-3"}>
+                        Waterbody restoration for cleaner, healthier ecosystems.
+                      </li>
+                      <li key={"donate-list-4"}>
+                        Educational programs to spread environmental awareness.
+                      </li>
+                    </ul>
+                  </div>
                   <DonateNow type="primary" />
                 </div>
               </ShadowContainer>
@@ -94,18 +104,24 @@ function InvolvedList() {
             <div className="relative md:right-14 top-0 w-full">
               <ShadowContainer shadowTo="right" theme={ColorType.primary_green}>
                 <div className="p-10">
-                  <h2 className="heading-2">Become A Partner</h2>
+                  <h2 className="heading-2">Let’s Build a Sustainable Future Together</h2>
                   <p className="pb-4">
-                    Collaboration is essential in driving sustainable change. If
-                    you represent an organization, institution, or community
-                    group that shares our vision, we invite you to become a
-                    partner.
+                    Partner with Seerakku to amplify your organization’s environmental impact. Collaborate on initiatives like corporate volunteering, tree plantation drives, and eco-education programs to demonstrate your commitment to sustainability.
                   </p>
-                  <p className="pb-4">
-                    By working together, we can leverage our collective
-                    resources, knowledge, and networks to implement impactful
-                    projects and create a greener and healthier future.
-                  </p>
+                  <div className="mb-4">
+                    <p className="font-bold mb-2">Why Partner with Us?</p>
+                    <ul className="list-inside list-disc">
+                      <li key={"parter-list-1"}>
+                        Enhance your CSR initiatives with impactful projects.
+                      </li>
+                      <li key={"parter-list-2"}>
+                        Engage employees in meaningful volunteering activities.
+                      </li>
+                      <li key={"parter-list-3"}>
+                        Showcase your organization’s dedication to environmental stewardship.
+                      </li>
+                    </ul>
+                  </div>
                   <ButtonLink type="primary_green" href={BECOME_PARTNER_LINK}>Become A Partner</ButtonLink>
                 </div>
               </ShadowContainer>
