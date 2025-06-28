4d746181d4512878fe22aa2e8b42237bacfc5e33
Sudharshan R <sudharshanrms2004@gmail.com>
2025-06-28 14:26:12 +0530

    Background image added for donor component

--- src/components/buttons/type.ts
+++ src/components/buttons/type.ts
@@ -3,6 +3,7 @@ export const ButtonType = {
   primary_green: "primary_green",
   secondary_light_green: "secondary_light_green",
   secondary_forest: "secondary_forest",
+  white: "white",
   void: "void",
 };
 
@@ -13,6 +14,7 @@ export enum ButtonFill {
   primary_green = "bg-primary-green text-white",
   secondary_light_green = "bg-secondary-light-green text-black",
   secondary_forest = "bg-secondary-forest text-white",
+  white = "bg-white text-black",
   void = "",
 }
 
@@ -21,5 +23,6 @@ export enum ButtonOutline {
   primary_green = "outline outline-primary-green text-primary-green bg-transparent",
   secondary_light_green = "outline outline-secondary-light-green text-secondary-light-green bg-transparent",
   secondary_forest = "outline outline-secondary-forest text-secondary-forest bg-transparent",
+  white = "outline outline-white text-black bg-transparent",
   void = "",
 }
--- src/features/our_focus/components/Donor.tsx
+++ src/features/our_focus/components/Donor.tsx
@@ -1,15 +1,23 @@
 import DonateNow from "@/components/buttons/DonateNow";
+import { donor_bg } from "../assets";
+import Image from "next/image";
+import ImageTint from "@/components/ImageTint";
 
 function Donor() {
   return (
     <section className="layout-section-xl max-md:p-0">
-      <div className="bg-primary p-12">
-        <div className="lg:w-2/3">
-          <h2 className="heading-2">Your Contribution Fuels Change</h2>
-          <p className="mb-5">
-            Every rupee you donate helps us restore ecosystems, empower communities, and combat climate change. From planting trees to supporting underprivileged farmers, your generosity drives our mission forward.
-          </p>
-          <DonateNow type="secondary_forest" icon />
+      <div className="relative p-12 bg-[url(/assets/images/donor_bg.jpg)]">
+        {/* <div className="absolute w-full h-full top-0 left-0 bg-red-100 z-0 overflow-hidden">
+          <Image src={donor_bg} alt="Donor backgroud image" className="w-full" />
+        </div> */}
+        <ImageTint className="z-0">
+        </ImageTint>
+        <div className="lg:w-2/3 relative w-full h-full text-white">
+            <h2 className="heading-2">Your Contribution Fuels Change</h2>
+            <p className="mb-5 font-medium text-lg">
+              Every rupee you donate helps us restore ecosystems, empower communities, and combat climate change. From planting trees to supporting underprivileged farmers, your generosity drives our mission forward.
+            </p>
+            <DonateNow type="white" icon />
         </div>
       </div>
     </section>
